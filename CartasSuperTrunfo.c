7508dbf73f3d4557bc266128331a0fcc89a33354
Vinicius Panema de Oliveira <panema.vinicius@gmail.com>
2026-04-29 01:50:26 +0000

    adicionei printf e scanf para estado e codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,31 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    char carta;
+    char estado;
+    char codigo[2];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+
   // Área para entrada de dados
 
+    printf("digite uma letra de 'A a H' para o estado: ");
+    scanf(" %c", &estado);
+    
+    printf("Digite o codigo: ");
+    scanf("%s", &codigo);
+
+
   // Área para exibição dos dados da cidade
+
+      printf("\nestado: %c", estado);
+      printf("\ncodigo: %c%s\n",estado,codigo);
+  
+
 
 return 0;
 } 
